026a3fc540cd16ed10b2d25e1c4c3c8f30a47a04
Md Mehedi Hasan <mehedihasan.hr.324@gmail.com>
2023-01-23 00:17:25 +0600

    configure firebase.ts

--- services/firebase/firebase.ts
+++ services/firebase/firebase.ts
@@ -14,19 +14,6 @@ let firebaseConfig = {
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-let productionFirebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-};
-
-if (env === "production") {
-    firebaseConfig = _.merge(firebaseConfig, productionFirebaseConfig);
-}
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
